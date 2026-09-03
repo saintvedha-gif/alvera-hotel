7e097fac011930ec6fbe722007387236f3a244a3
Vedha Saint <saintvedha@gmail.com>
2026-09-03 14:39:46 -0500

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+  basePath: "/alvera-hotel",
+  assetPrefix: "/alvera-hotel/",
 };
 
 export default nextConfig;
